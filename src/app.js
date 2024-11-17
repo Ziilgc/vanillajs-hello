83c5a52a0ba6fc1e9defa7ef00b33c391eca9348
Ángel Sánchez Moreno <univ.angel97@gmail.com>
2024-11-17 19:57:46 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  document.querySelector("#excusa").innerHTML = GenerateExcuse;
   console.log("Hello Rigo from the console!");
 };
+function GenerateExcuse() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoN = Math.floor(Math.random() * who.length);
+  let actionN = Math.floor(Math.random() * action.length);
+  let whatN = Math.floor(Math.random() * what.length);
+  let whenN = Math.floor(Math.random() * when.length);
+
+  return whoN + actionN + whatN + whenN;
+}
